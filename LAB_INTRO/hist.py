dca8d6799b5896052b58bc5556264a0b4eddac5b
Salvador Torpes <ist1102474@tecnico.ulisboa.pt>
2023-05-04 16:53:06 +0100

    C1S

---
+++ LAB_INTRO/hist.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+
+file1 = open('valoreshist_test_OneShot.txt', 'r')
+Lines = file1.readlines()
+
+x = []
+
+for values in Lines: 
+    x.append(float(values))
+
+for values in Lines:
+    sum += values
+
+print("Média = ")
+
+plt.hist(x,bins = 20)
+plt.show()
