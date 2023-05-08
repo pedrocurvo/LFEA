c9934b33c01837f6111602effdf59a93c862b218
Sofia Tété <sofia.garcia.nunes@gamil.com>
2023-05-08 10:07:24 +0100

    desvio padrao

--- LAB_INTRO/hist.py
+++ LAB_INTRO/hist.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from math import sqrt
 
 file1 = open('valoreshist_test_OneShot.txt', 'r')
 Lines = file1.readlines()
@@ -12,8 +13,10 @@ for values in Lines:
     sum += float(values)
     cardinal += 1
     
-
-print("Média = ", sum/cardinal)
+media = sum/cardinal
+desvio_padrao = sqrt(media)
+print("Média = ", media)
+print("Desvio Padrão =", desvio_padrao)
 
 plt.hist(x,bins = 20)
 plt.show()
