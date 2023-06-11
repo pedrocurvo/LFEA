314d31e276c8623ab287c2271d169bb42725947f
pedrocurvo <95446270+pedrocurvo@users.noreply.github.com>
2023-06-11 20:06:14 +0100

    Beta

---
+++ Beta/src/Read.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <utility>
+using namespace std;
+
+void ReadFile(const std::string& filename, vector<pair<double, double>>& data)
+{
+    //vector<pair<double, double>> data;
+
+    ifstream file(filename);
+    if (file.is_open())
+    {
+        std::string line;
+        while (getline(file, line))
+        {   
+            size_t pos = line.find(",");
+            while(pos != std::string::npos)
+            {
+                line.replace(pos, 1, ".");
+                pos = line.find(",", pos + 1);
+            }
+            istringstream iss(line);
+            double value1, value2;
+
+            if (iss >> value1 >> value2)
+            {
+                data.emplace_back(value1, value2);
+            }
+        }
+
+        file.close();
+    }
+    else
+    {
+        cout << "Failed to open the file." << endl;
+    }
+
+    
+    //return data;
+}
---
+++ Beta/src/Read.h
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <utility>
+using namespace std;
+void ReadFile(const std::string&, vector<pair<double, double>>&);
